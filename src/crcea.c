dd84e4adc1c71343ea2fc09a093a4616e2ebf342
dearblue <dearblue@users.noreply.github.com>
2017-07-14 23:50:19 +0900

    内部の表記を crcea に修正

--- src/crcea.c
+++ src/crcea.c
@@ -6,140 +6,140 @@
  *
  * Preprocessor definisions before included this file:
  *
- * [CRC_NO_MALLOC]
+ * [CRCEA_NO_MALLOC]
  *      Optional, not defined by default.
  *
- * [CRC_DEFAULT_MALLOC]
+ * [CRCEA_DEFAULT_MALLOC]
  *      Optional.
  */
 
 #include "../include/crcea.h"
 
-#if !defined(CRC_NO_MALLOC) && !defined(CRC_DEFAULT_MALLOC)
-#   define CRC_DEFAULT_MALLOC crc_default_malloc
-static void *CRC_DEFAULT_MALLOC(crc_t *cc, size_t size);
-#elif defined(CRC_DEFAULT_MALLOC)
-void *CRC_DEFAULT_MALLOC(crc_t *cc, size_t size);
+#if !defined(CRCEA_NO_MALLOC) && !defined(CRCEA_DEFAULT_MALLOC)
+#   define CRCEA_DEFAULT_MALLOC crcea_default_malloc
+static void *CRCEA_DEFAULT_MALLOC(crcea_t *cc, size_t size);
+#elif defined(CRCEA_DEFAULT_MALLOC)
+void *CRCEA_DEFAULT_MALLOC(crcea_t *cc, size_t size);
 #endif
 
-#if defined(CRC_ONLY_INT32)
-#   define CRC_PREFIX      crc32
-#   define CRC_TYPE        uint32_t
+#if defined(CRCEA_ONLY_INT32)
+#   define CRCEA_PREFIX      crcea32
+#   define CRCEA_TYPE        uint32_t
 #   include "../include/crcea/core.h"
 
-#   define CRC_SWITCH_BY_TYPE(C, F)     \
-        F(uint32_t, crc32);             \
+#   define CRCEA_SWITCH_BY_TYPE(C, F)     \
+        F(uint32_t, crcea32);             \
 
-#elif defined(CRC_ONLY_INT16)
-#   define CRC_PREFIX      crc16
-#   define CRC_TYPE        uint16_t
+#elif defined(CRCEA_ONLY_INT16)
+#   define CRCEA_PREFIX      crcea16
+#   define CRCEA_TYPE        uint16_t
 #   include "../include/crcea/core.h"
 
-#   define CRC_SWITCH_BY_TYPE(C, F)     \
-        F(uint16_t, crc16);             \
+#   define CRCEA_SWITCH_BY_TYPE(C, F)     \
+        F(uint16_t, crcea16);             \
 
-#elif defined(CRC_ONLY_INT8)
-#   define CRC_PREFIX      crc8
-#   define CRC_TYPE        uint8_t
+#elif defined(CRCEA_ONLY_INT8)
+#   define CRCEA_PREFIX      crcea8
+#   define CRCEA_TYPE        uint8_t
 #   include "../include/crcea/core.h"
 
-#   define CRC_SWITCH_BY_TYPE(C, F)     \
-        F(uint8_t, crc8);               \
+#   define CRCEA_SWITCH_BY_TYPE(C, F)     \
+        F(uint8_t, crcea8);               \
 
-#elif defined(CRC_ONLY_INT64)
-#   define CRC_PREFIX      crc64
-#   define CRC_TYPE        uint64_t
+#elif defined(CRCEA_ONLY_INT64)
+#   define CRCEA_PREFIX      crcea64
+#   define CRCEA_TYPE        uint64_t
 #   include "../include/crcea/core.h"
 
-#   define CRC_SWITCH_BY_TYPE(C, F)     \
-        F(uint64_t, crc64);             \
+#   define CRCEA_SWITCH_BY_TYPE(C, F)     \
+        F(uint64_t, crcea64);             \
 
 #else
-#   define CRC_PREFIX      crc8
-#   define CRC_TYPE        uint8_t
+#   define CRCEA_PREFIX      crcea8
+#   define CRCEA_TYPE        uint8_t
 #   include "../include/crcea/core.h"
 
-#   define CRC_PREFIX      crc16
-#   define CRC_TYPE        uint16_t
+#   define CRCEA_PREFIX      crcea16
+#   define CRCEA_TYPE        uint16_t
 #   include "../include/crcea/core.h"
 
-#   define CRC_PREFIX      crc32
-#   define CRC_TYPE        uint32_t
+#   define CRCEA_PREFIX      crcea32
+#   define CRCEA_TYPE        uint32_t
 #   include "../include/crcea/core.h"
 
-#   define CRC_PREFIX      crc64
-#   define CRC_TYPE        uint64_t
+#   define CRCEA_PREFIX      crcea64
+#   define CRCEA_TYPE        uint64_t
 #   include "../include/crcea/core.h"
 
 #   if 0 && HAVE_TYPE_UINT128_T
-#      define CRC_PREFIX  crc128
-#      define CRC_TYPE    uint128_t
+#      define CRCEA_PREFIX  crcea128
+#      define CRCEA_TYPE    uint128_t
 #      include "../include/crcea/core.h"
 
-#      define CRC_HAVE_TYPE_UINT128_T_CASE \
-       case CRC_TYPE_INT128:               \
-           F(uint128_t, crc128);           \
+#      define CRCEA_HAVE_TYPE_UINT128_T_CASE \
+       case CRCEA_TYPE_INT128:               \
+           F(uint128_t, crcea128);           \
            break;                          \
 
 #   else
-#      define CRC_HAVE_TYPE_UINT128_T_CASE
+#      define CRCEA_HAVE_TYPE_UINT128_T_CASE
 #   endif
 
-#   define CRC_SWITCH_BY_TYPE(C, F)         \
+#   define CRCEA_SWITCH_BY_TYPE(C, F)         \
         switch (C->inttype) {               \
-        case CRC_TYPE_INT8:                 \
-            F(uint8_t, crc8);               \
+        case CRCEA_TYPE_INT8:                 \
+            F(uint8_t, crcea8);               \
             break;                          \
-        case CRC_TYPE_INT16:                \
-            F(uint16_t, crc16);             \
+        case CRCEA_TYPE_INT16:                \
+            F(uint16_t, crcea16);             \
             break;                          \
-        case CRC_TYPE_INT32:                \
-            F(uint32_t, crc32);             \
+        case CRCEA_TYPE_INT32:                \
+            F(uint32_t, crcea32);             \
             break;                          \
-        case CRC_TYPE_INT64:                \
-            F(uint64_t, crc64);             \
+        case CRCEA_TYPE_INT64:                \
+            F(uint64_t, crcea64);             \
             break;                          \
-        CRC_HAVE_TYPE_UINT128_T_CASE        \
+        CRCEA_HAVE_TYPE_UINT128_T_CASE        \
         default:                            \
             break;                          \
         }                                   \
 
-#endif /* CRC_ONLY_UINT*** */
+#endif /* CRCEA_ONLY_UINT*** */
 
-crc_int
-crc_setup(crc_t *cc, crc_int crc)
+crcea_int
+crcea_setup(crcea_t *cc, crcea_int crc)
 {
-#define CRC_SETUP(T, P) return P ## _setup(cc, crc)
+#define CRCEA_SETUP(T, P) return P ## _setup(cc, crc)
 
-    CRC_SWITCH_BY_TYPE(cc, CRC_SETUP);
+    CRCEA_SWITCH_BY_TYPE(cc, CRCEA_SETUP);
 
-    return ~(crc_int)0;
+    return ~(crcea_int)0;
 }
 
-crc_int
-crc_update(crc_t *cc, const void *p, const void *pp, crc_int state)
+crcea_int
+crcea_update(crcea_t *cc, const void *p, const void *pp, crcea_int state)
 {
-#define CRC_UPDATE(T, P) return P ## _update(cc, p, pp, state)
+#define CRCEA_UPDATE(T, P) return P ## _update(cc, p, pp, state)
 
-    CRC_SWITCH_BY_TYPE(cc, CRC_UPDATE);
+    CRCEA_SWITCH_BY_TYPE(cc, CRCEA_UPDATE);
 
     return state;
 }
 
-crc_int
-crc_finish(crc_t *cc, crc_int state)
+crcea_int
+crcea_finish(crcea_t *cc, crcea_int state)
 {
-#define CRC_FINISH(T, P) return P ## _finish(cc, state)
+#define CRCEA_FINISH(T, P) return P ## _finish(cc, state)
 
-    CRC_SWITCH_BY_TYPE(cc, CRC_FINISH);
+    CRCEA_SWITCH_BY_TYPE(cc, CRCEA_FINISH);
 
-    return ~(crc_int)0;
+    return ~(crcea_int)0;
 }
 
-#ifndef CRC_NO_MALLOC
+#ifndef CRCEA_NO_MALLOC
 static void *
-CRC_DEFAULT_MALLOC(crc_t *cc, size_t size)
+CRCEA_DEFAULT_MALLOC(crcea_t *cc, size_t size)
 {
-    return CRC_MALLOC(size);
+    return CRCEA_MALLOC(size);
 }
 #endif
