fb7776d11d4e47ac1b84625b28d7f63af928755e
dearblue <dearblue@users.noreply.github.com>
2018-02-02 20:24:39 +0900

    プロトタイプ宣言の追加

--- include/crcea.h
+++ include/crcea.h
@@ -10,8 +10,11 @@
 
 #include "crcea/defs.h"
 
+size_t crcea_tablesize(const crcea_context *cc);
+int crcea_prepare_table(crcea_context *cc);
 crcea_int crcea_setup(crcea_context *cc, crcea_int crc);
 crcea_int crcea_update(crcea_context *cc, const void *src, const void *srcend, crcea_int state);
 crcea_int crcea_finish(crcea_context *cc, crcea_int state);
+crcea_int crcea(crcea_context *cc, const void *src, const void *srcend, crcea_int crc);
 
 #endif /* CRCEA_H__ */
