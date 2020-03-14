40f9214ebfe1ce66035b305b0ef97751c41177b6
dearblue <dearblue@users.osdn.me>
2020-03-14 15:23:02 +0900

    メールアドレスの変更

--- include/crcea/_bitcombine.h
+++ include/crcea/_bitcombine.h
@@ -1,7 +1,7 @@
 /**
  * @file _bitcombine.h
  * @brief 汎用 CRC 生成器
- * @author dearblue <dearblue@users.noreply.github.com>
+ * @author dearblue <dearblue@users.osdn.me>
  * @license Creative Commons Zero License (CC0 / Public Domain)
  */
 
--- include/crcea/core.h
+++ include/crcea/core.h
@@ -1,7 +1,7 @@
 /**
  * @file core.h
  * @brief 汎用 CRC 生成器
- * @author dearblue <dearblue@users.noreply.github.com>
+ * @author dearblue <dearblue@users.osdn.me>
  * @license Creative Commons Zero License (CC0 / Public Domain)
  *
  * This is a general CRC generator.
