19931b02f637a4e4bccdb2b2fe906dcace058b48
dearblue <dearblue@users.osdn.me>
2020-03-14 15:21:48 +0900

    `#include "crcea.h"` するだけの include/libcrcea.h ファイルを追加

---
+++ include/libcrcea.h
@@ -0,0 +1,8 @@
+/**
+ * @file libcrcea.h
+ * @brief 汎用 CRC 生成器
+ * @author dearblue <dearblue@users.noreply.github.com>
+ * @license Creative Commons Zero License (CC0 / Public Domain)
+ */
+
+#include "crcea.h"
